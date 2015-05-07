abe81d2393881d4d294c76d25207ea9bf8db45d7
kenpeter <figo2476@hotmail.com>
2015-05-07 10:52:06 +1000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Track quick tab with Google Analytics
---
+++ js/local.js
@@ -0,0 +1,36 @@
+(function ($) {
+  Drupal.behaviors.track_quick_tab = {
+    attach: function(context, settings) {
+			$(document).ready(function() {
+				
+				console.log('hi');
+
+				var selector = "ul.quicktabs-tabs li a";
+				// http://api.jquery.com/on/
+				$(selector).on("click", function() {
+					var protocol = window.location.protocol;
+					var uri = $(this).attr('href');
+					// http://stackoverflow.com/questions/1368264/how-to-get-host-name-in-javascript
+					var host = window.location.hostname; 
+					var url = protocol + '//' + host + uri; 
+					var text = $(this).text();
+
+					console.log('-start-');
+  				console.log(url);
+					console.log(text);
+			
+					dataLayer.push({
+						'event': 'arts_staff_quick_tab_click',
+						'arts_staff_main_page_url': url,
+						'arts_staff_main_page_title': text 
+					});
+
+				});
+			});
+		}
+	}
+
+	// Define your func
+
+
+})(jQuery);
